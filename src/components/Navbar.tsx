5ec5116f0c5830a19f5a57f9e92994e048a1fa8a
Kevin <kevinglennon01@gmail.com>
2023-05-27 13:09:00 +0100

    Rough navbar complete

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import navStyles from '../Styling/navBar/navStyles.module.scss';
+import React from "react";
+import navStyles from "../Styling/navBar/navStyles.module.scss";
 
 function Navbar() {
   return (
     <>
-        <nav className={navStyles.navContainer}>
-            <h1 className={navStyles.navLogo}>Logo</h1>
-            <div className={navStyles.navLinkWrapper}>
-                <ul className={navStyles.navUL}>
-                    <li className={navStyles.navLink}>
-                    Home
-                    </li>
-                    <li className={navStyles.navLink}>
-                    About Me
-                    </li>
-                    <li className={navStyles.navLink}>
-                        Contact Me
-                    </li>
-                </ul>
-            </div>
-        </nav>
+      <nav className={navStyles.navContainer}>
+        <h1 className={navStyles.navLogo}>Logo</h1>
+        <div className={navStyles.navLinkWrapper}>
+          <ul className={navStyles.navUL}>
+            <li className={navStyles.navLink}>Home</li>
+            <li className={navStyles.navLink}>About Me</li>
+            <li className={navStyles.navLink}>Projects</li>
+            <li className={navStyles.navLink}>Contact Me</li>
+          </ul>
+        </div>
+      </nav>
     </>
   );
 }
