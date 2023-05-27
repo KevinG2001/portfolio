17a51ac51fa7a6513f248e4e4fb167be8de8ffc9
Kevin <kevinglennon01@gmail.com>
2023-05-27 13:42:40 +0100

    Rough home section complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import './App.css';
-import  Navbar  from './components/navbar';
+import "./App.css";
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
-      <Navbar />
+      <Home />
     </>
   );
 }
---
+++ src/components/Home.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import homeStyles from "../Styling/homeStyles.module.scss";
+import Navbar from "./Navbar";
+import LinkedInImg from "../assests/linkedin.png";
+import GithubImg from "../assests/GitHub-Mark.png";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <div className={homeStyles.homeContainer}>
+        <div className={homeStyles.homeLeft}>
+          <h1>Software Engineer</h1>
+          <p>
+            Hi, I'm Kevin Glennon a software engineer based in Dublin, Ireland.
+          </p>
+          <div className={homeStyles.imgContainer}>
+            <a href="https://www.linkedin.com/in/kevin-glennon-98b511227/">
+              <img
+                src={LinkedInImg}
+                alt="LinkedIn"
+                className={homeStyles.homeImg}
+              />
+            </a>
+            <a href="https://github.com/KevinG2001">
+              <img
+                src={GithubImg}
+                alt="Github"
+                className={homeStyles.homeImg}
+              />
+            </a>
+          </div>
+        </div>
+        <div className={homeStyles.homeRight}>
+          <div className={homeStyles.homeFaceImg}></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
