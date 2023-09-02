f0cc8f7c536f2cca161c8b61cbd18f970a889a72
Kevin <kevinglennon01@gmail.com>
2023-09-02 23:21:57 +0100

    Removing old imports

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,6 +1,4 @@
-import Styles from "../Styling/pages/projects.module.scss";
 import HairsalonProject from "../components/Projects/HairsalonProject";
-import Teamproject from "../components/Projects/Teamproject";
 
 function Projects() {
   return (
