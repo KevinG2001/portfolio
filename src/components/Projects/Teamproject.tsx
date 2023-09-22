84903ca48232d0e2f3152a58fe8710d8a8ee781a
Kevin <kevinglennon01@gmail.com>
2023-09-22 16:49:05 +0100

    Made wrapper and moved title outside it

--- src/components/Projects/Teamproject.tsx
+++ src/components/Projects/Teamproject.tsx
@@ -5,25 +5,28 @@ function Teamproject() {
   return (
     <>
       <div className={Styles.container}>
-        <img src="" alt="Img" className={Styles.projectImg} />
-        <div className={Styles.descriptionWrapper}>
-          <h1 className={Styles.title}>Food and Drink Website</h1>
-          <div className={Styles.paragraph}>
-            This is a project I made in college for a group project. It is a
-            drink website that recommends drinks depending on the users drinks.
-          </div>
-          <div className={Styles.paragraph}>
-            The frontend is made with React.js and TypeScript and styled with
-            SASS.
-          </div>
-          <div className={Styles.paragraph}>
-            We used Node.js and express.js to create our own REST API and we
-            hosted a MySQL database using Microsoft Azure.
-          </div>
-          <div className={Styles.btnWrapper}>
-            <Link to="" className={Styles.btn}>
-              View Code (Link needed)
-            </Link>
+        <h1 className={Styles.title}>Food and Drink Website</h1>
+        <div className={Styles.wrapper}>
+          <img src="" alt="Img" className={Styles.projectImg} />
+          <div className={Styles.descriptionWrapper}>
+            <div className={Styles.paragraph}>
+              This is a project I made in college for a group project. It is a
+              drink website that recommends drinks depending on the users
+              drinks.
+            </div>
+            <div className={Styles.paragraph}>
+              The frontend is made with React.js and TypeScript and styled with
+              SASS.
+            </div>
+            <div className={Styles.paragraph}>
+              We used Node.js and express.js to create our own REST API and we
+              hosted a MySQL database using Microsoft Azure.
+            </div>
+            <div className={Styles.btnWrapper}>
+              <Link to="" className={Styles.btn}>
+                View Code (Link needed)
+              </Link>
+            </div>
           </div>
         </div>
       </div>
