5c5d03ff871f411e66b2063855c1f6ec0a6f267a
Kevin <kevinglennon01@gmail.com>
2024-04-23 17:04:25 +0100

    Changed homepage a bit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,18 @@
 import styles from "../Styling/pages/homeStyles.module.scss";
 import profilePic from "../assests/profilePic.jpeg";
-
+import planetImg from "../assests/planet-earth-svgrepo-com.svg";
 function Home() {
   return (
     <>
       <div className={styles.container}>
         <div className={styles.wrapper}>
+          <div className={styles.sidePanel} id={styles.left}>
+            <div>Located in Ireland</div>
+            <img src={planetImg} alt="" className={styles.planetImg} />
+          </div>
           <img src={profilePic} alt="" className={styles.profilePic} />
-          <div className={styles.content}>
-            <div>
-              Hi, <br /> Im Kevin <br />
-              Software Developer
-            </div>
+          <div className={styles.sidePanel} id={styles.right}>
+            <div>Kevin Glennon</div>
           </div>
         </div>
       </div>
