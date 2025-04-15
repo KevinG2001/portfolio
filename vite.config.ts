0c9d83717f2830ddd54a4bb65c25a73fb8dac015
Kevin <kevinglennon01@gmail.com>
2025-04-15 19:20:25 +0100

    Fixing build location

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist",
+    outDir: "dist", // Output the build files to the 'dist' directory
   },
-  base: "/portfolio/",
+  base: "/",
 });
